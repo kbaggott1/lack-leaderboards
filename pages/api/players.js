d98cb7efda399dac8a5db8234dd3c1afba915564
kbaggott1 <kevin.baggott1@gmail.com>
2024-01-11 18:14:32 -0500

    Use edge runtime

--- pages/api/players.js
+++ pages/api/players.js
@@ -1,17 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { firestore } from "../../lib/firebase.js"
 import { collection, getDocs } from "firebase/firestore";
-
-export default async function handler(req, res) {
+export default async function handler() {
   const playerCollection = collection(firestore, 'players')
 
   try {
     const querySnapshot = await getDocs(playerCollection);
     const players = querySnapshot.docs.map(doc => doc.data())
-    res.status(200).json(players)
+    return Response.json(players)
   }
   catch(error) {
     console.error("Error fetching players:", error);
-    res.status(500).json({ error: error.message });
+    return Response.json({ error: error.message });
   }
 }
+
+export const runtime = 'edge';
