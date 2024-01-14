43bf5390765a3258ba36d1febc6d45c6d04b57e8
kbaggott1 <kevin.baggott1@gmail.com>
2024-01-14 14:41:26 -0500

    Test new response

--- pages/api/player/[playerId].js
+++ pages/api/player/[playerId].js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
         Response.error({ error: 'Error updating player.' });
       }
     }
+    Response.error('something went wrong')
    }
-
    export const runtime = 'edge';
