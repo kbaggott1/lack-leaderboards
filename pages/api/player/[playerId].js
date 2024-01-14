c721fc2a27004c0be2b0647db7077b24ad35e3fd
kbaggott1 <kevin.baggott1@gmail.com>
2024-01-14 14:37:04 -0500

    Test fix

--- pages/api/player/[playerId].js
+++ pages/api/player/[playerId].js
@@ -2,30 +2,28 @@ import { firestore } from "@/lib/firebase";
 import { doc, setDoc, collection, updateDoc } from "firebase/firestore";
 
 export default async function handler(req, res) {
-   if (req.method === 'PUT') {
-     try {
-       const playerId = req.nextUrl.searchParams.get(['playerId']);
-       const playerData = await req.json();
-       const playerRef = doc(firestore, "players", playerId)
+    if (req.method === 'PUT') {
+      try {
+        //console.log(req.nextUrl.searchParams.get(['playerId']))
+        const playerId = req.nextUrl.searchParams.get(['playerId']);
+        const playerData = await req.json();
+        // Update the player in Firestore
+        const playerRef = doc(firestore, "players", playerId)
 
-       await updateDoc(playerRef, {
-         name: playerData.name,
-         lacks: playerData.lacks.toString(),
-         userId: playerData.userId
-       });
-  
-       // Send success response
-       res.json({ message: 'Player updated successfully.' });
-     } catch (error) {
-       // Handle error
-       console.error(error);
-       res.status(500).json({ error: 'Error updating player.' });
-     }
-   } else {
-     // Handle any other HTTP method
-     res.setHeader('Allow', ['PUT']);
-     res.status(405).end(`Method ${req.method} Not Allowed`);
+        await updateDoc(playerRef, {
+          name: playerData.name,
+          lacks: playerData.lacks.toString(),
+          userId: playerData.userId
+        });
+   
+        // Send success response
+        Response.json({ message: 'Player updated successfully.' });
+      } catch (error) {
+        // Handle error
+        console.error(error);
+        Response.error({ error: 'Error updating player.' });
+      }
+    }
    }
-}
 
-export const runtime = 'edge';
+   export const runtime = 'edge';
