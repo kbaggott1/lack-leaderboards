d2db98aa4130c986b03ff4bda13a53c444486f6b
kbaggott1 <kevin.baggott1@gmail.com>
2024-01-29 09:56:41 -0500

    Chnage theme color

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#fff" />
+        <meta name="theme-color" content="#09091b" />
       </Head>
       <body>
         <Main />
